6675565d5bf5155b3e608bef99bfaaf389df4d10
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-08-08 00:02:36 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,30 @@
+class Dog 
+  def initialize(name,breed)
+    @name 
+    @breed
+  end 
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+end
+
+ def breed=(dog_breed)
+  end
+  
+  def breed
+    @breed
+  end 
+end 
+
+fido = Dog.new("Fido", "Pug")
+fido.name = "Fido"
+fido.breed = "Pug"
+
+
+puts fido.breed
+
+
